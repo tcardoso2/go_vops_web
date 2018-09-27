c31d3accade240d3da38143d8d339669e743b32c
Tiago Cardoso <tiago.cardoso@jeppesen.com>
2018-09-27 09:36:46 +0000

    Fixed folder infrastructure and tests

--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 An old personal project revamp from .NET to Go  
 
 Version History:
-- 0.0.1: First version with a basic HTTP server and a GET api call (WIP); run using "go run main.go";  
-  
-TODO:  
-- use the tests suite  
+- 0.0.2: Created a proper folder structure. Fixed first test. Need logs and installation notes / and or script?  
+- 0.0.1: First version with a basic HTTP server and a GET api call (WIP); run using "go run main.go"; need to fix tests;   
---
+++ app/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/tcardoso2/go_vops_web/api"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/api/echo", echo)
+	http.HandleFunc("/api/timer", timer)
+	fmt.Print("Server running on port " + port())
+	http.ListenAndServe(port(), nil)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8081"
+	}
+	return ":" + port
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "go vops - version 0.1")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0]
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprintf(w, message)
+}
+
+func timer(w http.ResponseWriter, r *http.Request) {
+	t, err := json.Marshal(api.Timer{Name: "My timer", NowTime: 0})
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.Write(t)
+}
